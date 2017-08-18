bea4cacc4c3a55a49caf5e709ee12671663fa3b4
dmitryk-dk <kozlovdmitriyy@gmail.com>
2017-08-19 00:10:54 +0300

    add config for db

--- server.go
+++ server.go
@@ -7,6 +7,11 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"github.com/dmitryk-dk/blog/models"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"flag"
+	"github.com/dmitryk-dk/blog/config"
+	"log"
 )
 
 
@@ -24,6 +29,7 @@ type ResponseErr struct {
 }
 
 var posts map[int]*models.Post
+var configFile = flag.String("dbconfig", "dbconfig.json", "Path to config file")
 
 func indexHandler (w http.ResponseWriter, r *http.Request) {
 	post := &models.Post{
@@ -50,23 +56,35 @@ func dependenciesHandler () http.Handler {
 }
 
 func postHandler (w http.ResponseWriter, r *http.Request) {
-	var post models.Post
-
-	body,err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if r.Method == "POST" {
+		var post models.Post
+		body,err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		err = json.Unmarshal(body, &post)
+		if err != nil {
+			errorResp := &ResponseErr{"You don't create post"}
+			jsonErrResponse, err := json.Marshal(errorResp)
+			w.WriteHeader(http.StatusInternalServerError)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			w.Write(jsonErrResponse)
+		}
+		posts[post.Id] = &post
+		ok := &ResponseOk{ Ok: true }
+		jsonResponse, err := json.Marshal(ok)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		w.Write(jsonResponse)
+	} else {
+		errorMethod := &ResponseErr{"You use error method"}
+		jsonErrResponse, _ := json.Marshal(errorMethod)
+		w.Write(jsonErrResponse)
+		http.Error(w, "Used another Method", http.StatusInternalServerError)
 	}
-	json.Unmarshal(body, &post)
-
-	posts[post.Id] = &post
-	ok := &ResponseOk{ Ok: true }
-	jsonResponse, err := json.Marshal(ok)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	w.Write(jsonResponse)
 }
 
 func deleteHandler (w http.ResponseWriter, r *http.Request) {
@@ -92,14 +110,38 @@ func deleteHandler (w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonResponse)
 }
 
+func dbConfigReader () (*config.Config, error){
+	flag.Parse()
+	cfg, err := config.Read(*configFile)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
+
 func main () {
 	const port = "3030"
+	flag.Parse()
+	cfg, err := dbConfigReader()
+	if err != nil {
+		log.Fatalf("Error reading config %s: %s", *configFile, err)
+	}
 	posts = make(map[int]*models.Post, 0)
+	db, err := sql.Open(cfg.DbDriverName, cfg.User+":"+cfg.Password+"@"+cfg.Host+"/"+cfg.DbName)
 	depHandler := dependenciesHandler()
 	http.Handle("/dist/", depHandler)
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/post", postHandler)
 	http.HandleFunc("/delete", deleteHandler)
+	if err != nil {
+		panic(err)
+	}
+	res, err := db.Query("SELECT * FROM `post`")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v\n", res)
+	db.Close()
 	fmt.Printf("Running server on port: %s\n Type Ctr-c to shutdown server.\n", port)
 
 	http.ListenAndServe(":"+port, nil)
